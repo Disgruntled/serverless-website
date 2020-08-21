7ced76bb543c05228768d10639d181a5c24eb0af
Disgruntled <liam.wadman@gmail.com>
2020-08-21 13:06:48 -0700

    Adding trip page

--- worker.js
+++ worker.js
@@ -16,6 +16,26 @@ addEventListener('fetch', event => {
             return response
         }
 
+        if  (parsedUrl.pathname === "/calvin.jpg")
+        {
+          /*kv_variables is a reference to my cloudflare KV store, the variable is set through some CF magic */
+            const calvin = await kv_variables.get("calvin")
+            let response = new Response(calvin)
+            response.headers.set('Content-Type', 'text/html; Charset=UTF-8')
+            response.headers.set('Server', 'cloudflare-workers')
+            response.headers.set('Connection', 'keep-alive')
+            return response
+        }
+ 
+        if  (parsedUrl.pathname === "/trip.html")
+        {
+          /*kv_variables is a reference to my cloudflare KV store, the variable is set through some CF magic */
+            const styles = await kv_variables.get("trip")
+            let response = new Response(styles)
+            response.headers.set('content-type', 'text/html; Charset=UTF-8')
+            return response
+        }
+
     const index = await kv_variables.get("index")
     let response = new Response(index)
     response.headers.set('Content-Type', 'text/html; Charset=UTF-8')
